b1ad0717773c99d980f976ec763d09aa14cc2549
RavdeepKaur <40484476+RavdeepKaur@users.noreply.github.com>
2023-05-31 16:51:09 -0400

    Update 6. Receiving Input From User.php

--- 6. Receiving Input From User.php
+++ 6. Receiving Input From User.php
@@ -2,7 +2,7 @@
     <body>
        <!-- <form action="inputFromUserAction.php" method="post">-->
             
-       <form action="inputFromUserAction.php" method="get">Enter a number: <input type="text" name="number1">
+       <form action="6.inputFromUserAction.php" method="get">Enter a number: <input type="text" name="number1">
             <input type="Submit">
         </form>
     </body>
